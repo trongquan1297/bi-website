1c9b4705a431f91ba7e6520eef8b70ff4d81c2cc
quann <quannt@appotapay.com>
2025-04-29 14:21:43 +0700

    fix url

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ import {
 } from "lucide-react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
-const API_BASE_URL = process.env.BI_API_URL || "https://bi-dev.appotapay.com"
+const API_BASE_URL = process.env.BI_API_URL || "http://appoyapay-bi-api"
 
 interface Dashboard {
   id: number
